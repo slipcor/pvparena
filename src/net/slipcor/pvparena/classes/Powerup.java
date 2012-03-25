ccf176801015e719c0c5a0b3af02df57541b8711
NodinChan <nodinchan@live.com>
2012-03-25 14:47:36 +0800

    @slipcor can move them around and add stuff before I have time to work on it more

---
+++ src/net/slipcor/pvparena/classes/Powerup.java
@@ -0,0 +1,56 @@
+package net.slipcor.pvparena.classes;
+
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+/**
+ * Powerup Class
+ * 
+ * -
+ * 
+ * For powerups to extend
+ * 
+ * @author NodinChan
+ *
+ */
+public class Powerup {
+	
+	private final String name;
+	
+	private Effect[] effects;
+	
+	private Material[] materials;
+	
+	public Powerup(String name, Effect[] effects, Material[] materials) {
+		this.name = name;
+		this.effects = effects;
+		this.materials = materials;
+	}
+	
+	public void use(Player player) {}
+	
+	public void applyEffects(Player player) {
+		for (Effect effect : effects) {
+			effect.apply(player);
+		}
+	}
+	
+	public Effect[] getEffects() {
+		return effects;
+	}
+	
+	public final Material[] getMaterials() {
+		return materials;
+	}
+	
+	public final String getName() {
+		return name;
+	}
+	
+	public final void tick() {
+		for (Effect effect : effects) {
+			effect.tick();
+		}
+	}
+}
