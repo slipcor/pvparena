9df5a7b72eaa8d513c5bf8b59ed1fe013e412ab6
Chris Fehling <chris@slipcor.de>
2012-10-18 02:15:58 +0200

    v0.9.3.31 - quick fix for a sleeping NPE

--- src/net/slipcor/pvparena/classes/PACheck.java
+++ src/net/slipcor/pvparena/classes/PACheck.java
@@ -242,7 +242,7 @@ public class PACheck {
 			
 			ArenaGoal commGoal = null;
 			
-			for (ArenaGoal mod : PVPArena.instance.getAgm().getTypes()) {
+			for (ArenaGoal mod : arena.getGoals()) {
 				res = mod.checkJoin(sender, res, args);
 				if (res.getPriority() > priority && priority >= 0) {
 					// success and higher priority
