86ed8d7c290af1a520e9d1c189ad9ff7687aadc1
Chris Fehling <chris@slipcor.de>
2012-11-26 00:53:20 +0100

    v0.9.9.9 - add /pa togglemodule/!tm [module] !!!

---
+++ src/net/slipcor/pvparena/commands/PAA_ToggleMod.java
@@ -0,0 +1,60 @@
+package net.slipcor.pvparena.commands;
+
+import net.slipcor.pvparena.PVPArena;
+import net.slipcor.pvparena.arena.Arena;
+import net.slipcor.pvparena.core.Help;
+import net.slipcor.pvparena.core.Language;
+import net.slipcor.pvparena.core.Help.HELP;
+import net.slipcor.pvparena.core.Language.MSG;
+import net.slipcor.pvparena.loadables.ArenaModule;
+import org.bukkit.command.CommandSender;
+
+/**
+ * <pre>PVP Arena ACTIVATE Command class</pre>
+ * 
+ * A command to activate modules
+ * 
+ * @author slipcor
+ * 
+ * @version v0.9.9
+ */
+
+public class PAA_ToggleMod extends PAA__Command {
+
+	public PAA_ToggleMod() {
+		super(new String[0]);
+	}
+
+	@Override
+	public void commit(Arena arena, CommandSender sender, String[] args) {
+		if (!this.hasPerms(sender, arena)) {
+			return;
+		}
+
+		if (!this.argCountValid(sender, arena, args,
+				new Integer[]{1})) {
+			return;
+		}
+
+		// pa [arenaname] togglemod [module]
+		
+		String name = args[0].toLowerCase();
+		ArenaModule am = PVPArena.instance.getAmm().getModule(name);
+		if (am != null) {
+			am.toggleActivity(arena);
+			arena.msg(sender, Language.parse(MSG.SET_DONE, am.getName(), String.valueOf(am.isActive(arena))));
+			return;
+		}
+		arena.msg(sender, Language.parse(MSG.ERROR_UNKNOWN_MODULE, args[0]));
+	}
+
+	@Override
+	public String getName() {
+		return this.getClass().getName();
+	}
+
+	@Override
+	public void displayHelp(CommandSender sender) {
+		Arena.pmsg(sender, Help.parse(HELP.TOGGLEMOD));
+	}
+}
