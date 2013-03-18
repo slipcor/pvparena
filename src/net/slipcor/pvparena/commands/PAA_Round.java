1bd575ef52ba2b773c8095cc7a43f1daf8b2c6e5
slipcor <chris@slipcor.de>
2013-03-18 17:09:26 +0100

    v1.0.1.84 - properly pars /pa round arguments

--- src/net/slipcor/pvparena/commands/PAA_Round.java
+++ src/net/slipcor/pvparena/commands/PAA_Round.java
@@ -58,26 +58,26 @@ public class PAA_Round extends AbstractArenaCommand {
 		}
 		
 		try {
-			int round = Integer.parseInt(args[1]);
+			int round = Integer.parseInt(args[0]);
 			final PARoundMap roundMap = arena.getRounds();
 			
 			if (round >= arena.getRoundCount()) {
 				round = arena.getRoundCount();
 				
 				roundMap.set(round, new PARound(new HashSet<ArenaGoal>()));
-			} else if (args.length < 3) {
-				arena.msg(sender, Language.parse(MSG.ROUND_DISPLAY, args[1], StringParser.joinSet(roundMap.getGoals(round),", ")));
+			} else if (args.length < 2) {
+				arena.msg(sender, Language.parse(MSG.ROUND_DISPLAY, args[0], StringParser.joinSet(roundMap.getGoals(round),", ")));
 				return;
 			}
 			
 			ArenaGoal goal = null;
 			
-			if (args.length > 2) {
-				goal = PVPArena.instance.getAgm().getGoalByName(args[2].toLowerCase());
+			if (args.length > 1) {
+				goal = PVPArena.instance.getAgm().getGoalByName(args[1].toLowerCase());
 			}
 			
 			if (goal == null) {
-				arena.msg(sender, Language.parse(MSG.ERROR_GOAL_NOTFOUND, args[2], StringParser.joinSet(PVPArena.instance.getAgm().getAllGoalNames(), " ")));
+				arena.msg(sender, Language.parse(MSG.ERROR_GOAL_NOTFOUND, args[1], StringParser.joinSet(PVPArena.instance.getAgm().getAllGoalNames(), " ")));
 				arena.msg(sender, Language.parse(MSG.GOAL_INSTALLING));
 				return;
 			}
@@ -96,7 +96,7 @@ public class PAA_Round extends AbstractArenaCommand {
 			//TODO LATER
 			
 		} catch (NumberFormatException e) {
-			arena.msg(sender, Language.parse(MSG.ERROR_NOT_NUMERIC, args[1]));
+			arena.msg(sender, Language.parse(MSG.ERROR_NOT_NUMERIC, args[0]));
 			
 		} catch (Exception e) {
 			e.printStackTrace();
