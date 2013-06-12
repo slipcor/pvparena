27b51105a0aa1d0c448de75923076a82ddb0a1d6
slipcor <chris@slipcor.de>
2013-06-12 20:59:28 +0200

    v1.0.3.168 - little region tick rewrite

--- src/net/slipcor/pvparena/runnables/RegionRunnable.java
+++ src/net/slipcor/pvparena/runnables/RegionRunnable.java
@@ -2,6 +2,7 @@ package net.slipcor.pvparena.runnables;
 
 import org.bukkit.Bukkit;
 
+import net.slipcor.pvparena.PVPArena;
 import net.slipcor.pvparena.core.Debug;
 import net.slipcor.pvparena.loadables.ArenaRegionShape;
 import net.slipcor.pvparena.loadables.ArenaRegionShape.RegionType;
@@ -52,13 +53,37 @@ public class RegionRunnable implements Runnable {
 		 * 0 1 - 1 : no join region, game, tick for other region type
 		 * 1 0 - 1 : join region! no game! tick so ppl can join!
 		 * 1 1 - 1 : join region! game! tick so ppl can join!
-		 */
-		if (!region.getType().equals(RegionType.JOIN) && !region.getArena()
-				.isFightInProgress() && !region.getType().equals(RegionType.WATCH)) {
+		 * /
+		if (
+			!region.getType().equals(RegionType.JOIN) &&
+			!region.getArena().isFightInProgress() &&
+			!region.getType().equals(RegionType.WATCH)) {
 			Bukkit.getScheduler().cancelTask(iID);
 		} else {
 			region.tick();
+		}*/
+		
+		
+		if (region.getType() == RegionType.JOIN) {
+			// join region
+			if (region.getArena().isFightInProgress() &&
+					PVPArena.instance.getAgm().allowsJoinInBattle(region.getArena())) {
+				// ingame: only tick if allowed
+			region.tick();
+			} else {
+				// otherwise: no tick! No cancelling for join regions!
+				// Bukkit.getScheduler().cancelTask(iID);
+			}
+		} else if (region.getType() == RegionType.WATCH) {
+			// always tick for WATCH regions!
+			region.tick();
+		} else if (region.getArena().isFightInProgress()) {
+			// if ingame, always tick for other kinds of things!
+			region.tick();
+		} else {
+			// not ingame; ignore!
 		}
+		
 	}
 
 	public void setId(final int runID) {
