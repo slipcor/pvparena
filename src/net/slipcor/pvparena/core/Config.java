71eace5516e392494812b1dd1a63b754e149bdaf
slipcor <chris@slipcor.de>
2013-07-24 12:28:11 +0200

    v1.0.7.218 - add autovote autostart - so PA manages ALL THE SERVERS

--- src/net/slipcor/pvparena/core/Config.java
+++ src/net/slipcor/pvparena/core/Config.java
@@ -215,6 +215,7 @@ public class Config {
 		MODULES_ARENAMAPS_SHOWLIVES("modules.arenamaps.showlives", true),
 
 		MODULES_ARENAVOTE_EVERYONE("modules.arenavote.everyone", true),
+		MODULES_ARENAVOTE_AUTOSTART("modules.arenavote.autostart", false),
 		MODULES_ARENAVOTE_READYUP("modules.arenavote.readyup", 30),
 		MODULES_ARENAVOTE_ONLYSPAMTOJOIN("modules.arenavote.onlySpamToJOIN", false),
 		MODULES_ARENAVOTE_SECONDS("modules.arenavote.seconds", 30),
