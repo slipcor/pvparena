66db37322e53c307166aef610de23d5cb916c75a
Eredrim <Eredrim@users.noreply.github.com>
2022-02-02 21:10:42 +0100

    v1.15.2 - fix NPE if join sign is wrongly written

--- src/net/slipcor/pvparena/managers/ArenaManager.java
+++ src/net/slipcor/pvparena/managers/ArenaManager.java
@@ -389,15 +389,13 @@ public final class ArenaManager {
                     final String sName = sign.getLine(1).toLowerCase();
                     String[] newArgs = new String[0];
                     final Arena arena = ARENAS.get(sName);
-                    if (sign.getLine(2) != null
-                            && arena.getTeam(sign.getLine(2)) != null) {
+                    if (arena == null) {
+                        Arena.pmsg(player, Language.parse(MSG.ERROR_ARENA_NOTFOUND, sName));
+                        return;
+                    }
+                    if (sign.getLine(2) != null && arena.getTeam(sign.getLine(2)) != null) {
                         newArgs = new String[1];
                         newArgs[0] = sign.getLine(2);
-                    }
-                    if (arena == null) {
-                        Arena.pmsg(player,
-                                Language.parse(MSG.ERROR_ARENA_NOTFOUND, sName));
-                        return;
                     }
                     final AbstractArenaCommand command = new PAG_Join();
                     command.commit(arena, player, newArgs);
