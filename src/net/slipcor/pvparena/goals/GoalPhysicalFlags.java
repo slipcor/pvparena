31abae978992be5aa6231975e97f549b3e825aa2
Eredrim <Eredrim@users.noreply.github.com>
2022-05-07 18:11:14 +0200

    v1.15.3 - fix flag effect duration in PhysicalFlags

--- src/net/slipcor/pvparena/goals/GoalPhysicalFlags.java
+++ src/net/slipcor/pvparena/goals/GoalPhysicalFlags.java
@@ -311,7 +311,7 @@ public class GoalPhysicalFlags extends ArenaGoal implements Listener {
             return;
         }
 
-        player.addPotionEffect(new PotionEffect(pet, amp, 2147000));
+        player.addPotionEffect(new PotionEffect(pet, 2147000, amp));
     }
 
     @Override
